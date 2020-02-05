aa7f9a95a2191a89f0386e7dd64aa6cb10ce7bf8
Shahpar Islam <shahparislam@shahpars-mbp.nvstu.edu>
2020-02-04 21:19:51 -0500

    Define class attributs/methods and create a new object

--- pythonOOP1.py
+++ pythonOOP1.py
@@ -6,3 +6,21 @@ class MyClass:
 	x = 5
 
 print(MyClass)
+
+#How to create a class with methods and attributes?
+
+class Person:
+	def __init__(self, name, age):
+		self.name = name
+		self.age = age
+
+	def sayName(self):
+		print(f'My name is {self.name}')
+
+
+#Instanciate an object
+
+newPerson = Person("Shahpar", 28)
+
+print(f'{newPerson.name} is {newPerson.age} years old')
+	
